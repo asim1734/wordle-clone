88d71f0673b160ea0c356efa9ce07069e96a06d1
asim1734 <asim.rupani@gmail.com>
2024-11-14 21:27:15 +0530

    Add function to validate user input
    User can only enter characters.
    All letters will be displayed as Uppercase

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const inputContainer = document.querySelector("#input-container");
 const inputGrid = [];
+var curRow = 0;
+
+function init(){
+    generateGrid();
+    inputValidation();
+    document.querySelector("#input-container").addEventListener('input', inputValidation);
+    document.addEventListener('keydown', (e) =>{
+        if (e.key === 'Enter' && inputGrid[curRow][4].value){
+            curUserWord = getInputWord();
+            curRow++;
+        }
+    })
+}
 
 function generateGrid() {
+    const inputContainer = document.querySelector("#input-container");
     for (let i = 0; i < 6; i++) {
         let currentGrid = []; 
         for (let j = 0; j < 5; j++) {
@@ -11,8 +24,30 @@ function generateGrid() {
             currentGrid.push(input);  
             inputContainer.appendChild(input); 
         }
-        inputGrid.push(currentGrid);  // Add the row to the main grid array
+        inputGrid.push(currentGrid);
     }
 }
 
-generateGrid();
+function inputValidation(){
+    const regex = /^[a-zA-Z]$/;
+    inputGrid.forEach(row =>{
+        row.forEach(input => {
+            if (regex.test(input.value)){
+                input.value = input.value.toUpperCase();
+            }
+            else{
+                input.value = "";
+            }
+        });
+    });
+}
+
+function getInputWord(){
+    var curUserWord = ""
+    inputGrid[curRow].forEach(letter => {
+        curUserWord += letter.value;
+    });
+    return curUserWord;
+}
+
+init()
