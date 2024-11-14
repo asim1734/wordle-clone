1147be7a129943bcc494b46ebe5e82e6e2f84ba7
asim1734 <asim.rupani@gmail.com>
2024-11-15 01:20:46 +0530

    More frontend refinement
    Add function to change focus when user enters a valid letter
    go to previous element when backspace is clicked
    dont allow user to change focus manually

--- script.js
+++ script.js
@@ -1,53 +1,88 @@
 const inputGrid = [];
 var curRow = 0;
+var rowIndex = 0;
+var answerWord = ""
 
-function init(){
+function init() {
     generateGrid();
 
-    document.querySelector("#input-container").addEventListener('input', (event) =>{
-        console.log(event.target);
+    inputGrid[0][0].focus();
+    document.querySelector("#input-container").addEventListener('input', (event) => {
         inputValidation(event.target);
     });
-    
-    document.addEventListener('keydown', (e) =>{
-        if (e.key === 'Enter' && inputGrid[curRow][4].value){
-            curUserWord = getInputWord();
-            curRow++;
+
+    document.querySelector("#input-container").addEventListener('mousedown', (event) => {
+        if (event.target.tagName === 'INPUT') {
+            event.preventDefault(); 
         }
-    })
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter' && inputGrid[curRow][4].value) {
+            curUserWord = getInputWord();
+            
+            curRow++;
+            rowIndex = 0;
+            if (curRow < 6) { 
+                inputGrid[curRow][0].focus();
+            }
+        } else if (e.key === 'Backspace') {
+            handleBackspace();
+        }
+    });
 }
 
 function generateGrid() {
     const inputContainer = document.querySelector("#input-container");
     for (let i = 0; i < 6; i++) {
-        let currentGrid = []; 
+        let currentGrid = [];
         for (let j = 0; j < 5; j++) {
             let input = document.createElement('input');
-            input.type = 'text';  
-            input.maxLength = 1;   
-            currentGrid.push(input);  
-            inputContainer.appendChild(input); 
+            input.type = 'text';
+            input.maxLength = 1;
+            input.tabIndex = -1;
+            currentGrid.push(input);
+            inputContainer.appendChild(input);
         }
         inputGrid.push(currentGrid);
     }
 }
 
-function inputValidation(input){
+function inputValidation(input) {
     const regex = /^[a-zA-Z]$/;
-    if (regex.test(input.value)){
+    if (regex.test(input.value)) {
         input.value = input.value.toUpperCase();
-    }
-    else{
-        input.value = "";
+        moveToNextInput();  
+    } else {
+        input.value = ""; 
     }
 }
 
-function getInputWord(){
-    var curUserWord = ""
+function getInputWord() {
+    var curUserWord = "";
     inputGrid[curRow].forEach(letter => {
         curUserWord += letter.value;
     });
     return curUserWord;
 }
 
-init()
+function moveToNextInput() {
+    if (rowIndex < 4) {
+        rowIndex++;
+        inputGrid[curRow][rowIndex].focus();
+    }
+}
+
+function handleBackspace() {
+    if (rowIndex > 0) {
+        inputGrid[curRow][rowIndex].value = "";
+        rowIndex--;
+        inputGrid[curRow][rowIndex].focus();
+        inputGrid[curRow][rowIndex].value = "";
+    } else if (rowIndex === 0) {
+        inputGrid[curRow][rowIndex].value = "";
+    }
+}
+
+// Initialize the grid
+init();
